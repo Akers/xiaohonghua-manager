25c4c0103fdc45423ec759840c1c672531d3c52e
liangwenxu <liangwenxu@sinosoftgz.com.cn>
2023-07-07 18:06:36 +0800

    数据库设计

---
+++ admin/admin-server/src/main/java/com/akers/xiaohonghua/framework/security/handle/LogoutSuccessHandlerImpl.java
@@ -0,0 +1,52 @@
+package com.akers.xiaohonghua.framework.security.handle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import com.alibaba.fastjson2.JSON;
+import com.akers.xiaohonghua.common.constant.Constants;
+import com.akers.xiaohonghua.common.utils.ServletUtils;
+import com.akers.xiaohonghua.common.utils.StringUtils;
+import com.akers.xiaohonghua.framework.manager.AsyncManager;
+import com.akers.xiaohonghua.framework.manager.factory.AsyncFactory;
+import com.akers.xiaohonghua.framework.security.LoginUser;
+import com.akers.xiaohonghua.framework.security.service.TokenService;
+import com.akers.xiaohonghua.framework.web.domain.AjaxResult;
+
+/**
+ * 自定义退出处理类 返回成功
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler
+{
+    @Autowired
+    private TokenService tokenService;
+
+    /**
+     * 退出处理
+     * 
+     * @return
+     */
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException
+    {
+        LoginUser loginUser = tokenService.getLoginUser(request);
+        if (StringUtils.isNotNull(loginUser))
+        {
+            String userName = loginUser.getUsername();
+            // 删除用户缓存记录
+            tokenService.delLoginUser(loginUser.getToken());
+            // 记录用户退出日志
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(userName, Constants.LOGOUT, "退出成功"));
+        }
+        ServletUtils.renderString(response, JSON.toJSONString(AjaxResult.success("退出成功")));
+    }
+}
